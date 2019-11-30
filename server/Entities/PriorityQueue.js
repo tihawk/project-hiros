5e2d81d1a1475b2fe672888161f5b683f7aea491
tihawk <cccp.hawk@gmail.com>
2019-11-30 20:12:56 +0200

    refactored initiative queue class, and fixed bug closes #1

--- server/Entities/PriorityQueue.js
+++ server/Entities/PriorityQueue.js
@@ -1,8 +1,61 @@
+function sortBySpeedInitiativeAndPosition (elemA, elemB) {
+  const initiative = elemB.creature.stackMultiplier * elemB.creature.spd - elemA.creature.stackMultiplier * elemA.creature.spd
+  if (initiative < 0) return -1
+  if (initiative > 0) return 1
+  if (elemA.armyIndex > elemB.armyIndex) return 1
+  if (elemA.armyIndex < elemB.armyIndex) return -1
+  return 0
+}
+
+function swapElementsWithSameInitiativeIfPlayerAlreadyHadItsTurn (queue, lastPlayerLastRound, secondElem, firstElem) {
+  const initiative = secondElem.creature.stackMultiplier * secondElem.creature.spd - firstElem.creature.stackMultiplier * firstElem.creature.spd
+  if (initiative === 0) {
+    if (lastPlayerLastRound === firstElem.playerIndex && firstElem.playerIndex !== secondElem.playerIndex) {
+      queue.splice(0, 1, secondElem)
+      queue.splice(1, 1, firstElem)
+    }
+  }
+}
+
+function getInitiative (nextElem, elem) {
+  return nextElem.creature.stackMultiplier * nextElem.creature.spd - elem.creature.stackMultiplier * elem.creature.spd
+}
+
+function needsSwapToBeAlternating (prevElem, elem, nextElem) {
+  return prevElem.playerIndex === elem.playerIndex && elem.playerIndex !== nextElem.playerIndex
+}
+
+function findIndexOfNextWithSameInitiativeOfDifferentPlayer (elem, slicedQueue) {
+  let result = -1
+  for (let i = 0; i < slicedQueue.length + 1; i++) {
+    const candidate = slicedQueue.shift()
+    const initiative = getInitiative(candidate, elem)
+    if (initiative === 0) {
+      if (elem.playerIndex !== candidate.playerIndex) {
+        result = parseInt(i)
+        break
+      } else {
+        continue
+      }
+    } else {
+      result = -1
+      break
+    }
+  }
+  return result
+}
+
+function swapElements (queue, i, j, nextElem, elem) {
+  queue.splice(i, 1, nextElem)
+  queue.splice(j, 1, elem)
+}
+
 class PriorityQueue {
   constructor (board, players) {
     this.board = board
     this.players = players
     this.queue = []
+    this.queueCopy = []
     this.dequeued = []
 
     this.roundNum = 0
@@ -18,12 +71,14 @@ class PriorityQueue {
     const board = this.board
     for (const tileIndex in board) {
       if (board[tileIndex].hasCreature) {
-        this.queue.push({
-          tileIndex: parseInt(tileIndex),
+        const elemToPush = {
+          tileIndex: tileIndex,
           armyIndex: board[tileIndex].creature.armyIndex,
           playerIndex: this.players.indexOf(board[tileIndex].creature.player),
           creature: board[tileIndex].creature
-        })
+        }
+        this.queue.push({ ...elemToPush })
+        this.queueCopy.push({ ...elemToPush })
       }
     }
     this.updateQueue()
@@ -31,41 +86,40 @@ class PriorityQueue {
 
   updateQueue () {
     // sort by speed initiative and army position
-    this.queue.sort((elemA, elemB) => {
-      const initiative = elemB.creature.stackMultiplier * elemB.creature.spd - elemA.creature.stackMultiplier * elemA.creature.spd
-      if (initiative < 0) return -1
-      if (initiative > 0) return 1
-      if (elemA.armyIndex > elemB.armyIndex) return 1
-      if (elemA.armyIndex < elemB.armyIndex) return -1
-      return 0
-    })
+    this.queue.sort(sortBySpeedInitiativeAndPosition)
+
     // sort by making sure that in case of first two stacks have the same initiative, and are of different
     // players, first plays the player who didn't play last round (or in case of first round, player on the left)
     const firstElem = { ...this.queue[0] }
     const secondElem = { ...this.queue[1] }
-    const initiative = secondElem.creature.stackMultiplier * secondElem.creature.spd - firstElem.creature.stackMultiplier * firstElem.creature.spd
-    if (initiative === 0) {
-      if (this.lastPlayerLastRound === firstElem.playerIndex && firstElem.playerIndex !== secondElem.playerIndex) {
-        this.queue.splice(0, 1, secondElem)
-        this.queue.splice(1, 1, firstElem)
-      }
-    }
+    swapElementsWithSameInitiativeIfPlayerAlreadyHadItsTurn(
+      this.queue, this.lastPlayerLastRound, secondElem, firstElem)
+
     // sort by alternating players when they have stacks of same initiative
     for (let i = 1; i < this.queue.length - 1; i++) {
       const prevElem = this.queue[i - 1]
       const elem = this.queue[i]
       const nextElem = this.queue[i + 1]
-      const initiative = nextElem.creature.stackMultiplier * nextElem.creature.spd - elem.creature.stackMultiplier * elem.creature.spd
+      const initiative = getInitiative(nextElem, elem)
       if (initiative === 0) {
-        if (prevElem.playerIndex === elem.playerIndex && elem.playerIndex !== nextElem.playerIndex) {
-          console.log('swappint', this.queue[i - 1].playerIndex, this.queue[i - 1].creature.stackMultiplier, this.queue[i].playerIndex, this.queue[i].creature.stackMultiplier, this.queue[i + 1].playerIndex, this.queue[i + 1].creature.stackMultiplier)
-          this.queue.splice(i, 1, nextElem)
-          this.queue.splice(i + 1, 1, elem)
-          console.log('to', this.queue[i - 1].playerIndex, this.queue[i - 1].creature.stackMultiplier, this.queue[i].playerIndex, this.queue[i].creature.stackMultiplier, this.queue[i + 1].playerIndex, this.queue[i + 1].creature.stackMultiplier)
+        const index = findIndexOfNextWithSameInitiativeOfDifferentPlayer(elem, this.queue.slice(i)) + i
+        if (index - i !== -1) {
+          console.log(index)
+          if (needsSwapToBeAlternating(prevElem, elem, this.queue[index])) {
+            console.log('swappint', this.queue[i - 1].playerIndex, this.queue[i - 1].creature.stackMultiplier, this.queue[i].playerIndex, this.queue[i].creature.stackMultiplier, this.queue[index].playerIndex, this.queue[index].creature.stackMultiplier)
+            swapElements(this.queue, i, index, this.queue[index], elem)
+            console.log('to', this.queue[i - 1].playerIndex, this.queue[i - 1].creature.stackMultiplier, this.queue[i].playerIndex, this.queue[i].creature.stackMultiplier, this.queue[index].playerIndex, this.queue[index].creature.stackMultiplier)
+          } else {
+            console.log('smtin wrong with swapping logic')
+            console.log('didnt swap', this.queue[i - 1].playerIndex, this.queue[i - 1].creature.stackMultiplier, this.queue[i].playerIndex, this.queue[i].creature.stackMultiplier, this.queue[index].playerIndex, this.queue[index].creature.stackMultiplier)
+            console.log('to', this.queue[i - 1].playerIndex, this.queue[i - 1].creature.stackMultiplier, this.queue[i].playerIndex, this.queue[i].creature.stackMultiplier, this.queue[index].playerIndex, this.queue[index].creature.stackMultiplier)
+          }
         }
       }
     }
     this.lastPlayerLastRound = this.queue[this.queue.length - 1].playerIndex
+    const queueCopy = this.queue.map(elem => { return { plInd: elem.playerIndex, armInd: elem.armyIndex, stack: elem.creature.stackMultiplier } })
+    console.log('initiative for current round\n', queueCopy)
     return this.queue
   }
 
