c080834bf66a34b57c6451ca7770eaef3e731105
tihawk <cccp.hawk@gmail.com>
2019-12-23 02:12:04 +0200

    added fixed window frame

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ function App () {
   return (
     // <AdventureMap />
     <>
+      <div style={{
+        position: 'fixed',
+        zIndex: 100,
+        top: 0,
+        height: '2px',
+        width: '100vw',
+        backgroundColor: 'gold'
+      }}/>
       <RouterWrapper />
+      <div style={{
+        position: 'fixed',
+        zIndex: 100,
+        bottom: 0,
+        height: '2px',
+        width: '100vw',
+        backgroundColor: 'gold'
+      }}/>
     </>
   )
 }
